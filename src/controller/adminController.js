3d47d17f8006e9b816caca9fbb842ec0f8c6fe5b
bhoopendra3shukla <bhoopendrashukla3@gmail.com>
2024-04-03 12:06:17 +0530

    remove the unuse comments

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -1,43 +1,43 @@
 // Import required modules
-const db = require('../config/db'); // Import database configuration
-const Admin = db.Admin; // Import the Admin model from the database
-const { hashPassword, comparePassword, generateRandomNumber } = require('../helper/middleware'); // Import middleware functions for password hashing, comparison, and random number generation
-const jwt = require('jsonwebtoken'); // Import JSON Web Token module for authentication
-const secretKey = process.env.JWT_SECRET_KEY; // Get secret key from environment variables for JWT signing
-const CryptoJS = require('crypto-js'); // Import CryptoJS module for cryptographic functions
-const sendMail = require('../helper/email'); // Import function for sending emails
-const bcrypt = require('bcryptjs'); // Import bcryptjs module for password hashing and comparison
+const db = require('../config/db');
+const Admin = db.Admin; 
+const { hashPassword, comparePassword, generateRandomNumber } = require('../helper/middleware'); 
+const jwt = require('jsonwebtoken'); 
+const secretKey = process.env.JWT_SECRET_KEY; 
+const CryptoJS = require('crypto-js'); 
+const sendMail = require('../helper/email'); 
+const bcrypt = require('bcryptjs'); 
 
 // Function to handle admin login
 exports.adminLogin = async (req, res) => {
     try {
-        let { email, password } = req.body; // Destructure email and password from request body
-        let isAdminExists = await Admin.findOne({ email: email }); // Check if admin exists with the provided email
-        if (isAdminExists && isAdminExists !== null) { // If admin exists
-            let pass = isAdminExists.password; // Get hashed password from database
-            let checkPassword = await bcrypt.compare(password, pass); // Compare provided password with hashed password
-            if (checkPassword) { // If passwords match
-                const payload = { adminId: isAdminExists._id }; // Create payload for JWT token
-                const token = jwt.sign(payload, secretKey, { expiresIn: '1h' }); // Generate JWT token with expiration time of 1 hour
-                return res.status(200).send({ // Send success response with token
+        let { email, password } = req.body;
+        let isAdminExists = await Admin.findOne({ email: email });
+        if (isAdminExists && isAdminExists !== null) {
+            let pass = isAdminExists.password;
+            let checkPassword = await bcrypt.compare(password, pass);
+            if (checkPassword) {
+                const payload = { adminId: isAdminExists._id };
+                const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+                return res.status(200).send({
                     status: true,
                     msg: "admin login succesfully",
                     token: token
                 });
-            } else { // If passwords don't match
-                return res.status(200).send({ // Send failure response for invalid password
+            } else {
+                return res.status(200).send({
                     status: false,
                     msg: "Invalid password",
                 });
             }
-        } else { // If admin doesn't exist with provided email
-            return res.status(200).send({ // Send failure response for invalid email
+        } else {
+            return res.status(200).send({
                 status: false,
                 msg: "Invalid email",
             });
         }
-    } catch (error) { // Catch any errors that occur during execution
-        return res.status(500).send({ // Send error response for internal server error
+    } catch (error) {
+        return res.status(500).send({
             status: false,
             msg: "Something went wrong"
         });
@@ -47,82 +47,81 @@ exports.adminLogin = async (req, res) => {
 // Function to handle admin change password
 exports.adminChangePassword = async (req, res) => {
     try {
-        let id = req.decoded.adminId; // Get admin ID from JWT token
-        let { old_password, new_password } = req.body; // Destructure old_password and new_password from request body
-        let isAdminExists = await Admin.findOne({ _id: id }); // Check if admin exists with the provided ID
-        if (isAdminExists && isAdminExists !== null) { // If admin exists
-            let getOldPassword = isAdminExists.password; // Get hashed password from database
-            let checkPassword = await bcrypt.compare(old_password, getOldPassword); // Compare provided old password with hashed password
-            if (checkPassword) { // If old passwords match
-                let newPassword = await hashPassword(new_password); // Hash the new password
-                const filter = { _id: id }; // Define filter for update operation
-                const update = { // Define update operation to set new password
+        let id = req.decoded.adminId;
+        let { old_password, new_password } = req.body;
+        let isAdminExists = await Admin.findOne({ _id: id });
+        if (isAdminExists && isAdminExists !== null) {
+            let getOldPassword = isAdminExists.password;
+            let checkPassword = await bcrypt.compare(old_password, getOldPassword);
+            if (checkPassword) {
+                let newPassword = await hashPassword(new_password);
+                const filter = { _id: id };
+                const update = {
                     $set: {
                         password: newPassword
                     },
                 };
-                const check = await Admin.updateOne(filter, update); // Update admin password
-                if (check) { // If password update is successful
-                    return res.status(200).send({ // Send success response
+                const check = await Admin.updateOne(filter, update);
+                if (check) {
+                    return res.status(200).send({
                         status: true,
                         msg: "Password Change Sucessfully",
                     });
-                } else { // If password update fails
-                    return res.status(200).send({ // Send failure response
+                } else {
+                    return res.status(200).send({
                         status: true,
                         msg: "Password not Change",
                     });
                 }
-            } else { // If old passwords don't match
-                return res.status(200).send({ // Send failure response for invalid password
+            } else {
+                return res.status(200).send({
                     status: false,
                     msg: "Invalid password",
                 });
             }
-        } else { // If admin doesn't exist with provided ID
-            return res.status(200).send({ // Send failure response for invalid admin
+        } else {
+            return res.status(200).send({
                 status: false,
                 msg: "Admin Not Exists",
             });
         }
-    } catch (error) { // Catch any errors that occur during execution
-        return res.status(500).send({ // Send error response for internal server error
+    } catch (error) {
+        return res.status(500).send({
             status: false,
             msg: "Something went wrong"
         });
     }
 };
 
-
 // Function to handle sending OTP for admin password reset
 exports.adminForgetPasswordSendOtpFn = async (req, res) => {
     try {
-        let { email } = req.body; // Extract email from request body
-        let isAdminExists = await Admin.findOne({ email: email }); // Check if admin exists with provided email
-        if (isAdminExists) { // If admin exists
-            const randomNumber = await generateRandomNumber(10000, 20000); // Generate a random number as OTP
-            const filter = { email: email }; // Define filter for admin
-            const update = { // Define update operation to set OTP code
+        let { email } = req.body;
+        let isAdminExists = await Admin.findOne({ email: email });
+        if (isAdminExists) {
+            const randomNumber = await generateRandomNumber(10000, 20000);
+            const filter = { email: email };
+            const update = {
                 $set: {
                     code: randomNumber
                 },
             };
-            const check = await Admin.updateOne(filter, update); // Update admin with new OTP code
-            if (check) { // If update is successful
-                let emailSendFunction = await sendMail.mail(email, randomNumber); // Send email with OTP
-                return res.status(200).send({ // Send success response
+            const check = await Admin.updateOne(filter, update);
+            if (check) {
+                let emailSendFunction = await sendMail.mail(email, randomNumber);
+                return res.status(200).send({
                     status: true,
                     msg: "otp send succesfully",
                 });
             }
-        } else { // If admin doesn't exist with provided email
-            return res.status(200).send({ // Send failure response
+        } else {
+            return res.status(200).send({
                 status: true,
                 msg: "Admin not found"
             });
         }
-    } catch (error) { // Catch any errors that occur during execution
-        return res.status(500).send({ // Send error response for internal server error
+    } catch (error) {
+        return res.status(500).send({
             status: false,
             msg: "Something went wrong"
         });
@@ -132,47 +131,46 @@ exports.adminForgetPasswordSendOtpFn = async (req, res) => {
 // Function to handle admin password reset
 exports.adminForgetPasswordFn = async (req, res) => {
     try {
-        let { email, otp, password } = req.body; // Extract email, OTP, and new password from request body
-        let isAdminExists = await Admin.findOne({ email: email }); // Check if admin exists with provided email
-        if (isAdminExists) { // If admin exists
-            let code = isAdminExists.code; // Get OTP code from admin record
-            if (code == otp) { // If provided OTP matches the stored OTP
-                let newPassword = await hashPassword(password); // Hash the new password
-                const filter = { email: email }; // Define filter for admin
-                const update = { // Define update operation to set new password
+        let { email, otp, password } = req.body;
+        let isAdminExists = await Admin.findOne({ email: email });
+        if (isAdminExists) {
+            let code = isAdminExists.code;
+            if (code == otp) {
+                let newPassword = await hashPassword(password);
+                const filter = { email: email };
+                const update = {
                     $set: {
                         password: newPassword
                     },
                 };
-                const check = await Admin.updateOne(filter, update); // Update admin with new password
-                if (check) { // If update is successful
-                    return res.status(200).send({ // Send success response
+                const check = await Admin.updateOne(filter, update);
+                if (check) {
+                    return res.status(200).send({
                         status: true,
                         msg: "Your Password Has Been Reset Succesfully",
                     });
-                } else { // If update fails
-                    return res.status(200).send({ // Send failure response
+                } else {
+                    return res.status(200).send({
                         status: true,
                         msg: "Your Password Not Be Reset",
                     });
                 }
-            } else { // If provided OTP does not match the stored OTP
-                return res.status(200).send({ // Send failure response
+            } else {
+                return res.status(200).send({
                     status: true,
                     msg: "Invalid otp",
                 });
             }
-        } else { // If admin doesn't exist with provided email
-            return res.status(200).send({ // Send failure response
+        } else {
+            return res.status(200).send({
                 status: true,
                 msg: "Admin not found"
             });
         }
-    } catch (error) { // Catch any errors that occur during execution
-        return res.status(500).send({ // Send error response for internal server error
+    } catch (error) {
+        return res.status(500).send({
             status: false,
             msg: "Something went wrong"
         });
     }
 };
-
